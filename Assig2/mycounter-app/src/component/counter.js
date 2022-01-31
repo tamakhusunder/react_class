7dd89b733ce9ae7b731813cfa775a2dd9733631d
tamakhusunder <tamakhusunder@gmail.com>
2022-01-31 22:34:47 +0545

    assign2:  setState(prevProps=> ({})) used

--- Assig2/mycounter-app/src/component/counter.js
+++ Assig2/mycounter-app/src/component/counter.js
@@ -10,17 +10,27 @@ class Counter extends Component{
     }
 
     increase = () => {
-        this.setState({count : this.state.count+1});
+        this.setState({
+            count : this.state.count+1
+        });
     }
 
     decrease = () => {
-        this.setState({count : (this.state.count-1<0 ? 0 : this.state.count-1)});
+        this.setState( (prevState) =>{
+            console.log(prevState.count);
+            console.log(this.state.count);
+            return {
+                count : (prevState.count-1<0 ? 0 : prevState.count-1)
+            };
+            // count : (this.state.count-1<0 ? 0 : this.state.count-1)
+        });
     }
 
     render() {
         return (
             <>
-                <div className='counter-title'>Counter : {this.state.count}</div>
+                <div className='counter-title'>Counter</div>
+                <div className='counter-num'>{this.state.count}</div>
                 <div>
                     <button onClick={this.decrease} className='btn-counter minus'>-</button>
                     <button onClick={this.increase} className='btn-counter plus'>+</button>
